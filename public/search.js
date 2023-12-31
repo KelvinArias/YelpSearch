b5c38f91c119bd8a12130a0077e2bc5faf30ac18
Kelvin Arias <kelvin727631@gmail.com>
2023-12-30 22:11:02 -0500

    moving the logo and search icon into the public folder

---
+++ public/search.js
@@ -0,0 +1,10 @@
+const Search = ({ color, size }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size}>
+    <path
+      fill={color}
+      d="M21.853 20.355l-3.444-3.443a9.428 9.428 0 10-16.761-6.171 9.428 9.428 0 0015.348 7.586l3.443 3.442a1 1 0 101.414-1.414zM5.82 16.245a7.429 7.429 0 115.253 2.175 7.38 7.38 0 01-5.253-2.176z"
+    ></path>
+  </svg>
+);
+
+export default Search;
